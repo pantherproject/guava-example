814d7c20fe02b3dbc846a41d576a3cb13f840cc9
panther.dongdong <panther.dongdong@qunar.com>
2015-11-13 23:25:02 +0800

    maven项目构建成功

---
+++ src/main/java/com/panther/test/Test.java
@@ -0,0 +1,16 @@
+package com.panther.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * test(maven项目建立成功的测试)
+ * Created by panther.dongdong on 2015/11/13.
+ */
+public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("maven 项目构建成功");
+    }
+}
