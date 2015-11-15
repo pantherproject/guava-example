7fc4b1aba1fdca01389fcf3df5c77cd9f38907fb
panther.dongdong <panther.dongdong@qunar.com>
2015-11-15 17:23:38 +0800

    java静态导入

---
+++ src/main/java/com/panther/sta/Routine.java
@@ -0,0 +1,12 @@
+package com.panther.sta;
+
+/**
+ * 常规条件下导入类
+ * Created by panther.dongdong on 2015/11/15.
+ */
+public class Routine {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.toHexString(42));
+    }
+}
